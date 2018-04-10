d03bee6701c06298379b4344fe3cc310cec8391f
Jul Jen <juliajen.prog@gmail.com>
2018-04-10 20:01:29 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,10 +1,10 @@
 class Helpers
 
-  def current_user # accepts the session hash as an argument
+  def self.current_user(session)# accepts the session hash as an argument
     # uses the user_id from the session hash to find the user in the database and return that user
   end
 
-  def is_logged_in? #accepts the session hash as an argument
+  def self.is_logged_in?(session) #accepts the session hash as an argument
     #returns true if the user_id is in the session hash and false if not.The Ruby !! operator will come in handy here.
   end
 

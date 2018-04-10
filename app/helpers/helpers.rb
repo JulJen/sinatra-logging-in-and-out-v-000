ee9259a2166163ebe84a16d63fa58bacd95e3f2d
Jul Jen <juliajen.prog@gmail.com>
2018-04-10 19:56:18 +0000

    automatically backed up by learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,11 +1,13 @@
 class Helpers
 
-  def current_user
+  def current_user # accepts the session hash as an argument
+    # uses the user_id from the session hash to find the user in the database and return that user
   end
 
-  def is_logged_in?
+  def is_logged_in? #accepts the session hash as an argument
+    #returns true if the user_id is in the session hash and false if not.The Ruby !! operator will come in handy here.
   end
-  
+
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end

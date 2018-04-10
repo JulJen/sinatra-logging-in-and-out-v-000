52b390d5976a9fa054a75ccef54d0f0f8a2fd839
Jul Jen <juliajen.prog@gmail.com>
2018-04-10 20:42:20 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,8 +14,7 @@ class ApplicationController < Sinatra::Base
     @user = User.find_by(username: params["username"], password: params["password"])
 
     if @user
-      Helpers.current_user(session) = @user.id
-      # session[:user_id] = @user.id
+      session[:user_id] = @user.id
       redirect '/account'
     else
       erb :error
